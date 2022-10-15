3b7a97b8efd8813d294400b542a115fe4dbfb917
Noah <zap.noah@gmail.com>
2022-10-15 12:36:17 -0500

    add all

---
+++ index.ts
@@ -0,0 +1,43 @@
+const accounts: Account[] = [];
+
+type Account = {
+  number: number;
+  balance: number;
+};
+
+// 1
+function open(number: number, balance: number): Account[] {
+  const account: Account = { number, balance };
+  accounts.push(account);
+  return accounts;
+}
+
+//2
+function getBalance(number: number): number {
+  const account = accounts.find((account) => account.number === number);
+  if (!account) throw new Error('La cuenta no se encontró');
+  return account.balance;
+}
+
+function deposit(number, amount) {
+  const account = accounts.find((account) => account.number);
+  if (!account) throw new Error('La cuenta no se encontró');
+
+  account.balance += amount;
+}
+
+function withdraw(number, amount) {
+  const account = accounts.find((account) => account.number);
+  if (!account) throw new Error('La cuenta no se encontró');
+
+  if (account.balance - amount < 0) throw new Error('Fondos insuficientes');
+  account.balance -= amount;
+}
+
+open('111', 0);
+
+deposit('111', 1000);
+console.log(getBalance('111'));
+
+withdraw('111', 500);
+console.log(getBalance('111'));
